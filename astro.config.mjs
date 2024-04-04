6fbf7260818cd1d198c2bc2d9cb33e3c99e9bfb2
Levi <levi@technomancy.dev>
2024-04-04 12:58:03 +0200

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind()],
+  site: "https://survive.technomancy.dev",
+  integrations: [mdx(), tailwind(), sitemap()],
   markdown: {
     shikiConfig: {
       // Choose from Shiki's built-in themes (or add your own)
